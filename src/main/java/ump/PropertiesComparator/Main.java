482b839f3d3353b8afa623556d460e32f4e08c3b
ImadEddine-Aouinti <imadeddineaouinti010@gmail.com>
2025-07-10 11:17:44 +0100

    le type HTML

--- src/main/java/ump/PropertiesComparator/Main.java
+++ src/main/java/ump/PropertiesComparator/Main.java
@@ -3,6 +3,8 @@ package ump.PropertiesComparator;
 import ump.PropertiesComparator.comparator.ComparatorFactory;
 import ump.PropertiesComparator.comparator.PropertiesComparator;
 import ump.PropertiesComparator.model.ComparisonResult;
+import ump.PropertiesComparator.report.ReportFactory;
+import ump.PropertiesComparator.report.ReportFormatter;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,35 +13,35 @@ public class Main {
     public static void main(String[] args) {
         // Création de données de test simulant deux fichiers .properties
         Map<String, String> props1 = new HashMap<>();
-        props1.put("key1", "A");
-        props1.put("key2", "B");
-        props1.put("key3", "C");
+        props1.put("key1", "value1");
+        props1.put("key2", "value2");
+        props1.put("key3", "value3");
 
         Map<String, String> props2 = new HashMap<>();
-        props2.put("key1", "A");
-        props2.put("key2", "X");
-        props2.put("key4", "D");
+        props2.put("key1", "value1");
+        props2.put("key2", "different_value");
+        props2.put("key4", "value4");
 
-        // Initialisation de la factory
-        ComparatorFactory factory = new ComparatorFactory();
+        // Initialisation des factories
+        ComparatorFactory comparatorFactory = new ComparatorFactory();
+        ReportFactory reportFactory = new ReportFactory();
 
         try {
-            // Obtention du comparateur pour simpleDiff
-            PropertiesComparator comparator = factory.getComparator(ComparatorFactory.ComparisonType.SIMPLE);
+            // Obtention du comparateur pour SimpleDiff
+            PropertiesComparator comparator = comparatorFactory.getComparator(ComparatorFactory.ComparisonType.SIMPLE);
 
             // Comparaison des deux maps
             ComparisonResult result = comparator.compare(props1, props2, "file1.properties", "file2.properties");
 
-            // Affichage des résultats
-            System.out.println("Comparaison entre " + result.getFile1() + " et " + result.getFile2() + ":");
-            Map<String, String> differences = result.getDiff();
-            if (differences.isEmpty()) {
-                System.out.println("Aucune différence trouvée.");
-            } else {
-                differences.forEach((key, diff) -> System.out.println("Clé: " + key + " -> " + diff));
-            }
+            // Obtention du formateur pour JSON
+            ReportFormatter formatter = reportFactory.createFormat(ReportFactory.FormatType.HTML);
+
+            // Formatage et affichage du rapport
+            String report = formatter.format(result);
+            System.out.println("Rapport JSON :");
+            System.out.println(report);
         } catch (IllegalArgumentException e) {
-            System.err.println("Erreur: " + e.getMessage());
+            System.err.println("Erreur : " + e.getMessage());
         }
     }
 }
--- src/main/java/ump/PropertiesComparator/report/ReportFactory.java
+++ src/main/java/ump/PropertiesComparator/report/ReportFactory.java
@@ -1,6 +1,7 @@
 package ump.PropertiesComparator.report;
 
 import ump.PropertiesComparator.model.ComparisonResult;
+import ump.PropertiesComparator.report.impl.HTMLFormat;
 import ump.PropertiesComparator.report.impl.JSONFormat;
 
 public class ReportFactory {
@@ -11,6 +12,8 @@ public class ReportFactory {
         switch(type){
             case JSON :
                 return new JSONFormat() ;
+            case HTML :
+                return new HTMLFormat() ;
             default:
                 throw new IllegalArgumentException("Type de format inconnu : " + type);
         }
