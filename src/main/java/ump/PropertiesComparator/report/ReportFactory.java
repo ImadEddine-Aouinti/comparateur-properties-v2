46237617396b8f98248cc25b5ca9b21d217e696f
ImadEddine-Aouinti <imadeddineaouinti010@gmail.com>
2025-07-10 10:56:44 +0100

    ajouty factory pour report

---
+++ src/main/java/ump/PropertiesComparator/report/ReportFactory.java
@@ -0,0 +1,18 @@
+package ump.PropertiesComparator.report;
+
+import ump.PropertiesComparator.model.ComparisonResult;
+import ump.PropertiesComparator.report.impl.JSONFormat;
+
+public class ReportFactory {
+    public enum FormatType{
+        JSON,HTML,XML
+    }
+    public ReportFormatter createFormat(FormatType type){
+        switch(type){
+            case JSON :
+                return new JSONFormat() ;
+            default:
+                throw new IllegalArgumentException("Type de format inconnu : " + type);
+        }
+    }
+}
