40619b3013034afbd9f2fb8e924e900dfbcd0141
ImadEddine-Aouinti <imadeddineaouinti010@gmail.com>
2025-07-31 15:54:39 +0100

    modifier la forme XML

--- src/main/java/ump/PropertiesComparator/report/impl/XMLFormat.java
+++ src/main/java/ump/PropertiesComparator/report/impl/XMLFormat.java
@@ -3,20 +3,24 @@ package ump.PropertiesComparator.report.impl;
 import ump.PropertiesComparator.model.ComparisonResult;
 import ump.PropertiesComparator.report.ReportFormatter;
 
+import static java.util.FormatProcessor.FMT;
+
 public class XMLFormat implements ReportFormatter {
     @Override
     public String format(ComparisonResult result){
         StringBuilder xml = new StringBuilder();
-        xml.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
-        xml.append("<comparator");
-        xml.append("<titre>Comparaison entre :").append(result.getFile1()).append(" et ").append(result.getFile2()).append("</titre>\n");
-        xml.append("<table>\n<tr>\n<th>Clé</th>\n<th>Différence</th>\n</tr>\n");
-        result.getDifferences().forEach((key,diff)->{
-            xml.append("<tr>\n<td>").append(key).append("</td>\n<td>").append(diff).append("</td>\n</tr>\n");
-        });
-        xml.append("</table>\n");
-        xml.append("</comparator>");
-        return xml.toString();
+        result.getDifferences().forEach((key, diff) ->
+                xml.append(FMT."<tr><td>\{key}</td><td>\{diff}</td></tr>\n")
+                );
+        return FMT."""
+                <?xml version=\\"1.0\\" encoding=\\"UTF-8\\"?>
+                <comparator>
+                <titre>Comparaison entre \{result.getFile1()} et \{result.getFile2()} :</titre>
+                <table>
+                \{xml}
+                </table>
+                </comparator>
+                """;
     }
 
 }
