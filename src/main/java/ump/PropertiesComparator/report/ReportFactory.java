d9c70f3f645596934dbc3f96030fdf272f9a7497
ImadEddine-Aouinti <imadeddineaouinti010@gmail.com>
2025-08-11 15:58:45 +0100

    remplace switch par une Map report

--- src/main/java/ump/PropertiesComparator/report/ReportFactory.java
+++ src/main/java/ump/PropertiesComparator/report/ReportFactory.java
@@ -1,24 +1,30 @@
 package ump.PropertiesComparator.report;
 
-import ump.PropertiesComparator.model.ComparisonResult;
 import ump.PropertiesComparator.report.impl.HTMLFormat;
 import ump.PropertiesComparator.report.impl.JSONFormat;
 import ump.PropertiesComparator.report.impl.XMLFormat;
 
+import java.util.Map;
+import java.util.function.Supplier;
+
 public class ReportFactory {
-    public enum FormatType{
-        JSON,HTML,XML
+
+    public enum FormatType {
+        JSON, HTML, XML
     }
-    public ReportFormatter createFormat(FormatType type){
-        switch(type){
-            case JSON :
-                return new JSONFormat() ;
-            case HTML :
-                return new HTMLFormat() ;
-            case XML :
-                return new XMLFormat() ;
-            default:
-                throw new IllegalArgumentException("Type de format inconnu : " + type);
+
+    private static final Map<FormatType, Supplier<ReportFormatter>> FORMATTERS = Map.of(
+            FormatType.JSON, JSONFormat::new,
+            FormatType.HTML, HTMLFormat::new,
+            FormatType.XML, XMLFormat::new
+    );
+
+    public ReportFormatter createFormat(FormatType type) {
+        if (type == null) {
+            throw new IllegalArgumentException("Le type de format ne peut pas être null");
         }
+        return FORMATTERS.getOrDefault(type, () -> {
+            throw new IllegalArgumentException("Type de format inconnu : " + type);
+        }).get();
     }
 }
